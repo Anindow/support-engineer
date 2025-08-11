99ebf7a4c0945659156efe6f2686002aba3608a0
Anindow Saikat <anindowsaikat@gmail.com>
2025-08-11 12:54:00 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import MagneticDownloadButton from "@/components/MagneticDownloadButton"
 
 export default function Resume() {
   return (
-    <div className='flex justify-center py-8 px-4 bg-gray-100 min-h-screen'>
+    <div className='flex justify-center py-8 px-4 bg-gray-100 min-h-screen overflow-auto'>
       <div className='fixed top-6 right-6 z-50'>
         <MagneticDownloadButton />
       </div>
